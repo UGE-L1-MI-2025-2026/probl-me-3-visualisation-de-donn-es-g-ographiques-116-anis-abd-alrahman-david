26ed59336025142222ed5be8447c0aee5b4e39f5
Anis-944 <anis.hamed@edu.univ-eiffel.fr>
2025-11-24 12:40:21 +0100

    ajout de departement

--- main.py
+++
@@ -1,7 +0,0 @@
-import shapefiles
-
-sf = shapefile.Reader("routes.shp")
-
-for shape, record in zip(sf.shapes(), sf.records()):
-    print("Route : ", record[0])
-    print("Coordonnées : ", shape.points[:5], "...")  # afficher les 5 premières
