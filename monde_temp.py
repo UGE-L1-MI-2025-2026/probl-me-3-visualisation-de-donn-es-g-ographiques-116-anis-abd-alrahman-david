d5ca16489f24e14e34fc7b31cb9b3fecc6dc7127
Abdel1039 <ghaliiiklz@gmail.com>
2025-12-08 12:44:49 +0100

    en cours

--- monde_temp.py
+++ monde_temp.py
@@ -1,8 +1,13 @@
 from netCDF4 import *
+import numpy as np
 
 nc = Dataset("DCENT_ensemble_1850_2023_ensemble_mean.nc", "r")
 
-print(nc.dimensions['lon'])
+print(nc.variables['temperature'])
 
-def couleur_monde():
-    
+def couleur_monde(min_x , min_y , max_x, max_y):
+    """
+    Cette fonction doit retourne des carre de couleurs transparent sur la fenetre
+    par dessus de la carte du monde
+    """
+    None
