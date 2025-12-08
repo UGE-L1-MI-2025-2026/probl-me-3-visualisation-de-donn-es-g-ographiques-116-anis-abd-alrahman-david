aa255809f5ef3f76b6325305b78aaf8455f9347a
Anis-944 <anis.hamed@edu.univ-eiffel.fr>
2025-12-08 12:52:00 +0100

    animation

--- animation.py
+++ animation.py
@@ -2,20 +2,49 @@ from temperature import *
 from afficher_carte import *
 from fltk import *
 
-def animation():
+
+
+
+
+def animation(date):
+    min = 1
+    max = 31
+    jour = date[-2:]
+    mois = date[5:7]
+    annee = date[0:4]
     while True:
         ev = donne_ev
         tev = type_ev
         if tev == 'Touche':
             nom_touche = touche(ev)
             if nom_touche == 'Left':
-                da = int(d) - 1
-                d = str(da)
-                temperature(d)
-                carte('france',d)
+                print('yes')
+                jour = date[-2]
+                int_jour = int(jour) - 1
+                if int_jour < min:
+                    int_jour = max
+                    jour = str(int_jour)
+                    date = '2018-07-{jour}'
+                else:
+                    jour = str(int_jour)
+                    date = '2018-07-{jour}'
+                temperature(date)
             elif nom_touche == 'Right':
-                da = int(d) + 1
-                d = str(da)
-                temperature(d)
-                carte('france',d)
-            
+                print('no')
+                jour = date[-2]
+                int_jour = int(jour) + 1
+                if int_jour > max:
+                    int_jour = min
+                    jour = str(int_jour)
+                    date = '2018-07-{jour}'
+                else:
+                    jour = str(int_jour)
+                    date = '2018-07-{jour}'
+                temperature(date)
+        elif tev == 'Quitte':
+            break
+        else:
+            pass
+
+    mise_a_jour()
+                
