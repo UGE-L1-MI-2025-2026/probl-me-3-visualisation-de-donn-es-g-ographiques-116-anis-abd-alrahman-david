727afe862f09ea3a9ea3fd48cb737b44ebe4086a
Anis-944 <anis.hamed@edu.univ-eiffel.fr>
2025-12-08 11:12:29 +0100

    main

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from animation import *
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
-
+OUTREMER = ['974', '972', '971', '973', '976']
+PROBLEME = ["69D", '69M']
 
 
 
 def main():
-    cree_fenetre(700)
+    carte('france')
+    animation()
+
+
